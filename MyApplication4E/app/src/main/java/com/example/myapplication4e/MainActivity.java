7822ab6cfc0c45319d68d2d97f3be27fae18c419
jihazard <yoonjh238@gmail.com>
2019-07-18 16:24:17 +0900

    image view

---
+++ MyApplication4E/app/src/main/java/com/example/myapplication4e/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication4e;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View viewImg = findViewById(R.id.iuImg);
+        viewImg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "THIS IS IU!!!!!!!", Toast.LENGTH_SHORT).show();
+    }
+}

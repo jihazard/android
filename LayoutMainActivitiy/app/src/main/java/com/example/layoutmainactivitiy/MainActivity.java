441d89f2b0e29642a03402bdcc504c97d489605f
jihazard <yoonjh238@gmail.com>
2019-07-22 14:46:45 +0900

    이벤트 인터페이스 OnClickListener , OnTouchListener

--- LayoutMainActivitiy/app/src/main/java/com/example/layoutmainactivitiy/MainActivity.java
+++ LayoutMainActivitiy/app/src/main/java/com/example/layoutmainactivitiy/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.layoutmainactivitiy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener ,View.OnTouchListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.iv_like).setOnClickListener(this);
+        findViewById(R.id.iv_share).setOnClickListener(this);
+        findViewById(R.id.iv_photo).setOnTouchListener(this);
+        findViewById(R.id.iv_photo).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_like :
+                Toast.makeText(this, "click like!!!!!!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.iv_share :
+                Toast.makeText(this, "click share!!!!!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.iv_photo :
+                Toast.makeText(this, "click share!!!!!", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Toast.makeText(this, "TOUCH DOWN", Toast.LENGTH_SHORT).show();
+                break;
+            case MotionEvent.ACTION_UP:
+                Toast.makeText(this, "TOUCH UP", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return false;
     }
 }

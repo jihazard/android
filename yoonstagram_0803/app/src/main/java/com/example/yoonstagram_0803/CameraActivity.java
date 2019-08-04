8a6386327d78c89b71b5e9a5a2d6ae5bd700289a
jihazard <yoonjh238@gmail.com>
2019-08-04 18:21:29 +0900

    ë¹uri -> bitmap->file 만들기

--- yoonstagram_0803/app/src/main/java/com/example/yoonstagram_0803/CameraActivity.java
+++ yoonstagram_0803/app/src/main/java/com/example/yoonstagram_0803/CameraActivity.java
@@ -3,13 +3,24 @@ package com.example.yoonstagram_0803;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -29,5 +40,32 @@ public class CameraActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public Bitmap getBitmap(Uri uri ) throws IOException {
+
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri,"r");
+
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+
+        return image;
+    }
+
+    public File createFileFromBitmap(Bitmap bitmap) throws FileNotFoundException {
+        File newfile = new File(getFilesDir(),"");
+        FileOutputStream fileOutputStream = new FileOutputStream(makeImagePath());
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,fileOutputStream);
+        return newfile;
+    }
+
+    public String makeImagePath() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_hhmmss");
+        Date date = new Date();
+        String strDate = simpleDateFormat.format(date);
+        return strDate +".png";
     }
 }
